0e7b4aedb40479e693795580d0b96c5eabd4b833
SabitovRatmir <sabitovratmir@rambler.ru>
2025-06-03 21:12:02 +0300

    Simplified the organization of data storage using Spring Data JDBC

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,11 +1,21 @@
 package tacos;
 
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-public class Ingredient {
-	private final String id;
+@Table
+@AllArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
+public class Ingredient implements Persistable<String> {
+	@Id
+	private String id;
 	private final String name;
 	private final Type type;
 
